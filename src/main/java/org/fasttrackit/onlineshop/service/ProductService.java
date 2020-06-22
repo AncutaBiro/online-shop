3a7b366ba98c6c1c05d998b0e65feecb8d3b5cbf
AncutaBiro <ancuta.biro@yahoo.com>
2020-06-22 21:08:22 +0300

    added getProduct by id method plus tests

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -1,6 +1,7 @@
 package org.fasttrackit.onlineshop.service;
 
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistence.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
@@ -8,11 +9,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 //Spring Bean (services, repositories etc.)
 @Service
 public class ProductService {
 
-    //lombok dependency
+    //lombok dependency for getter-setter etc. fast coding
 
     //factory dessing pattern
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
@@ -36,5 +39,21 @@ public class ProductService {
         product.setImageUrl(request.getImageUrl());
 
         return productRepository.save(product);
+    }
+
+    public Product getProduct (long id) {
+        LOGGER.info("Retrieving product {}", id);
+
+//        Optional<Product> productOptional = productRepository.findById(id);
+//
+//        if(productOptional.isPresent()) {
+//            return productOptional.get();
+//        } else {
+//            throw new ResourceNotFoundException("Product" + id + "not found.");
+//        }
+
+        return productRepository.findById(id)
+                //lambda expression
+                .orElseThrow(() -> new ResourceNotFoundException("Product" + id + "not found."));
     }
 }
